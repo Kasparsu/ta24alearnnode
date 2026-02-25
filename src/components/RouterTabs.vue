8c591f2dde1b2a933e52813901856afc5d2cb40f
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-02-25 12:48:44 +0200

    Add RouterTabs

--- src/components/RouterTabs.vue
+++ src/components/RouterTabs.vue
@@ -6,8 +6,6 @@
         <ul>
             <li class="is-active"><RouterLink to="/">ToDo</RouterLink></li>
             <li><RouterLink to="/modals">Modals</RouterLink></li>
-            
-            
         </ul>
     </div>
 </template>
