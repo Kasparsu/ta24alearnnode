cda960f05b2456e3851fbaa489c911bc296944d3
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-02-25 14:11:27 +0200

    Add messages page and message component

---
+++ src/pages/Messages.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Message from '../components/Message.vue';
+
+
+</script>
+<template>
+    <Message title="WOW SUCH TITLE">Lol <b>WTF</b></Message>
+    <Message >Lol <b>WTF</b></Message>
+    <Message color="primary" size="small">Lol <b>WTF</b></Message>
+    <Message color="info" size="medium">Lol <b>WTF</b></Message>
+    <Message color="bla" size="bla">Lol <b>WTF</b></Message>
+    
+</template>
